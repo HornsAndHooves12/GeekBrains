1305a579582db7ef4665b89f997503d45d67e856
Alexander <Spankyhamtony@gmail.com>
2021-11-02 18:59:33 +0200

    Add project files.

---
+++ ConsoleApp1/Lesson1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Lesson1 : IGeekBrains
+    {
+        public string LessonName => "Lesson 1";
+
+        public void Test()
+        {
+            // (имя, фамилия, возраст, рост, вес)
+            Console.Write("Имя: ");
+            var name = Console.ReadLine();
+            Console.Write("Фамилия: ");
+            var lastName = Console.ReadLine();
+            Console.Write("Возраст: ");
+            var age = Console.ReadLine();
+            Console.Write("Рост: ");
+            var height = Console.ReadLine();
+            Console.Write("Вес: ");
+            var weight = Console.ReadLine();
+
+            Console.WriteLine(name + " " + lastName + " " + age + " " + height + " " + weight);
+            Console.WriteLine($"{name} {lastName} {age} {height} {weight}");
+            Console.WriteLine("{0} {1} {2} {3} {4}", name, lastName, age, height, weight);
+
+            //////////////////////////////////////////////////////////////
+
+
+        }
+
+        
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lessons = new List<IGeekBrains> {
+                new Lesson1(),
+            };
+            lessonPosition(lessons).Test();
+        }
+
+        private static IGeekBrains lessonPosition(List<IGeekBrains> lessons)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"Выберите урок от 1 до {lessons.Count}");
+                    for (int i = 0; i < lessons.Count; i++)
+                    {
+                        var lesson = lessons[i];
+                        Console.WriteLine($"{i + 1} {lesson.LessonName}");
+                    }
+                    var number = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    return lessons[number - 1];
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Все херня, давай по новому");
+                }
+            }
+
+        }
+
+    }
+
+}
