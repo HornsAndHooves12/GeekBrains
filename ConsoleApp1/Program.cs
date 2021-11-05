0b1033512228a7b2c1c30029b1c36848f7a86f6c
Alexander <Spankyhamtony@gmail.com>
2021-11-05 12:00:24 +0200

    remove

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,11 @@
-﻿using ConsoleApp1.Common.Interfaces;
-using ConsoleApp1.L1;
-
+﻿
 namespace ConsoleApp1;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var lessons = new List<IGeekBrains> {
-                new Lesson1(),
-            };
-        lessonPosition(lessons).Test();
-    }
-
-    private static IGeekBrains lessonPosition(List<IGeekBrains> lessons)
-    {
-        while (true)
-        {
-            try
-            {
-                Console.WriteLine($"Выберите урок от 1 до {lessons.Count}");
-                for (int i = 0; i < lessons.Count; i++)
-                {
-                    var lesson = lessons[i];
-                    Console.WriteLine($"{i + 1} {lesson.LessonName}");
-                }
-                var number = int.Parse(Console.ReadLine());
-                Console.Clear();
-                return lessons[number - 1];
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Все херня, давай по новому");
-            }
-        }
+  
 
     }
 
