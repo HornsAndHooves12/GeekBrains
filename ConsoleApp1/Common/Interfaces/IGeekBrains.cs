eb988b52f8094d3792a8b396a61672fd2bebd187
4atg89 <spankyhamtony@gmail.com>
2021-11-14 10:00:49 +0200

    lesson 1 (#2)

---
+++ ConsoleApp1/Common/Interfaces/IGeekBrains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Common.Interfaces;
+
+internal interface IGeekBrains
+{
+    void Test();
+
+    string LessonName { get; }
+
+}
